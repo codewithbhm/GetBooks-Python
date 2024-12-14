108ea6b8afb79efde2a9401f653fd8b24e0e0dd5
Bəhruz Mamedov <144023215+codewithbhm@users.noreply.github.com>
2024-12-14 22:31:32 +0400

    Comments
    Signed-off-by: Bəhruz Mamedov <144023215+codewithbhm@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,44 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-homepage = 'http://books.goalkicker.com/'
+def download_books(homepage):
+    """Downloads books from the specified homepage."""
 
-with requests.session() as s:
+    with requests.Session() as session:
+        home_page = session.get(homepage)
+        soup = BeautifulSoup(home_page.content, 'html.parser')
 
-    home = s.get(homepage)
-    bowl = BeautifulSoup(home.content, 'html.parser')
+        book_urls = []
+        for book_container in soup.find_all('div', {'class': 'bookContainer grow'}):
+            for link in book_container.find_all('a'):
+                book_url = f"{homepage}{link['href']}"
+                book_urls.append(book_url)
 
-    # get the URLS for each book subpage from the homepage
+        for book_front in book_urls:
+            book_page = session.get(book_front)
+            book_soup = BeautifulSoup(book_page.content, 'html.parser')
 
-    book_urls = []
-
-    for books in bowl.find_all('div', {'class': 'bookContainer grow'}):
-        deepsearch = books.find_all('a')
-
-        for i in deepsearch:
-
-            book_url = homepage + i['href']
-            book_urls.append(book_url)
-
-    # get book download link from each subpage and download it ;-)
-
-    for book_front in book_urls:
-
-        book_site = s.get(book_front)
-        book_code = BeautifulSoup(book_site.content, 'html.parser')
-
-        for dummy in book_code.find_all('div', {'id': 'frontpage'}):
-            link_elem = dummy.findAll('a')
-
-            for link in link_elem:
-
-                download_payload = book_front + '/' + link['href']
-
-                if ('.pdf' in download_payload):
+            for link in book_soup.find('div', {'id': 'frontpage'}).find_all('a'):
+                download_payload = f"{book_front}/{link['href']}"
+                if '.pdf' in download_payload:
                     download_link = download_payload
+                    print(f"Downloading {link['href']}...")
 
-                    print('Downloading ' + link['href'] + ' ...')
-
-                    file = s.get(download_link)
+                    response = session.get(download_link)
                     with open(link['href'], 'wb') as f:
-                        f.write(file.content)
+                        f.write(response.content)
+
+if __name__ == "__main__":
+    homepage = 'http://books.goalkicker.com/'
+    download_books(homepage) 
+
